6c7ac13deb070c6c61c8f7856202de3719d9d5be
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2023-03-25 21:34:47 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,1 +1,8 @@
 print("ok")
+import yaml
+
+@app.route('/', methods=['GET'])
+  def index():
+  user = request.cookies.get('username')
+  # Insecure example
+  return "Hey there, {}!".format(yaml.unsafe_load(user))
