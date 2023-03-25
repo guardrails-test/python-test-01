c7b74f2e827ce10488a8997a016fa604f9b30b8c
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2023-03-25 21:36:23 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -6,3 +6,17 @@ import yaml
   user = request.cookies.get('username')
   # Insecure example
   return "Hey there, {}!".format(yaml.unsafe_load(user))
+
+import subprocess
+# ...
+@app.route("secure-command-execution-arg")
+def a():
+  arg = flask.request.args.get("arg")
+  # Secure example for passing and argument to a command
+  subprocess.run(["ping", "-c1", arg])
+# ...
+@app.route("secure-command-execution-cmd")
+def b():
+  cmd = flask.request.args.get("cmd")
+  arg = flask.request.args.get("arg")
+  allowed = {'p': "ping", 'l': "ls"}
